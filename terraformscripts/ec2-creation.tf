a3704af9c0d79ad95d189c6b6871ae6dd263d0f0
Srinu-web <112843932+Srinu-web@users.noreply.github.com>
2022-11-04 15:04:23 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,14 +1,14 @@
 provider "aws"{
-   region  = "ap-southeast-1" 
+   region  = "ap-south-1" 
    
 }
 
 resource "aws_instance" "AWSEC2Instance"{
      count   = 1
-     ami     =  "ami-051f0947e420652a9"
+     ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
-     key_name        = "devopsmss"
+     key_name        = "jenkinsserver"
      tags = {
         Name = "tomcatserver"
      }
